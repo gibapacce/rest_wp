dfe68bec773fd4425398dc233839d61b96148642
Giba Pacce <gibapacce@gmail.com>
2021-08-26 14:08:51 -0300

    transformado contato em WordPress

--- wp-content/themes/rest/page-contato.php
+++ wp-content/themes/rest/page-contato.php
@@ -8,27 +8,17 @@
 			<h2 class="subtitulo"><?php the_title(); ?></h2>
 
 			<div class="grid-16">
-				<a href="https://www.google.com.br/maps" target="_blank"><img src="img/rest-mapa.jpg" alt="Fachada do Rest"></a>
+				<a href="https://www.google.com.br/maps" target="_blank"><img src="<?php the_field('foto_maps_rest'); ?>" alt="<?php the_field('descricao_maps_rest'); ?>"></a>
 			</div>
 
+			<?php if(have_rows('informacao_contato')): while(have_rows('informacao_contato')) : the_row(); ?>
 			<div class="grid-1-3 contato-item">
-				<h2>Dados</h2>
-				<p>21 2422-9999</p>
-				<p>contato@rest.com</p>
-				<p>facebook.com/rest/</p>
+				<h2><?php the_sub_field('titulo_contato'); ?></h2>
+				<?php if(have_rows('dados_contato')): while(have_rows('dados_contato')) : the_row(); ?>
+					<p><?php the_sub_field('dado_contato'); ?></p>
+				<?php endwhile; else : endif; ?>
 			</div>
-			<div class="grid-1-3 contato-item">
-				<h2>Horários</h2>
-				<p>Segunda à Sexta: 10 às 23</p>
-				<p>Sábado: 14 às 23</p>
-				<p>Domingo: 14 às 22</p>
-			</div>
-			<div class="grid-1-3 contato-item">
-				<h2>Endereço</h2>
-				<p>Rua Marechal, 29</p>
-				<p>Copacabana - Rio de Janeiro</p>
-				<p>Brasil - Terra - Via Láctea</p>
-			</div>
+			<?php endwhile; else : endif; ?>
 		</section>
 <?php endwhile; else: endif; ?>
 
