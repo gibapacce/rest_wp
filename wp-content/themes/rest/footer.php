40ca773da7c4c15f1ea0f9b533923ba5be344ea4
Giba Pacce <gibapacce@gmail.com>
2021-08-31 09:11:02 -0300

    adicionado menu wp

--- wp-content/themes/rest/footer.php
+++ wp-content/themes/rest/footer.php
@@ -1,10 +1,12 @@
 		<footer>
 			<nav class="nav-footer">
-				<ul>
-					<li class="current_page_item"><a href="/origamid/testewp/">Menu</a></li>
-					<li><a href="/origamid/testewp/sobre/">Sobre</a></li>
-					<li><a href="/origamid/testewp/contato/">Contato</a></li>
-				</ul>
+				<?php
+						$args = array(
+							'menu' => 'principal',
+							'container' => false
+						);
+						wp_nav_menu( $args );
+					?>
 			</nav>
 
 			<p><?php bloginfo('name'); ?> © <?php echo date("Y"); ?>. Alguns direitos reservados.</p>
